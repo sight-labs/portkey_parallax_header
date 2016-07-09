6c033e1b3c23d4809a2f85a2dbad0bdb0e8bb04e
Elom Gomez <gomezelom@yahoo.com>
2016-07-08 23:49:42 -0500

    Add scroll view helpers

---
+++ lib/portkey_parallax_header/scroll_view_helper.rb
@@ -0,0 +1,47 @@
+class UIScrollView
+  attr_reader :portkey_parallax_header, :shows_parallax
+
+  # @param [UIView] view, the view being added
+  # @param [Float] height, the height of the parallax header
+  def add_portkey_parallax_with_view(view, height: height)
+    add_portkey_parallax_with_view(view, height: height, shadow: false, min_height: 0)
+  end
+
+  # @param [UIView] view, the view being added
+  # @param [Float] height, the height of the parallax header
+  # @param [Boolean] shadow, whether to show a shadow along the bottom or not
+  def add_portkey_parallax_with_view(view, height: height, shadow: shadow)
+    add_portkey_parallax_with_view(view, height: height, shadow: shadow, min_height: 0)
+  end
+
+  # @param [UIView] view, the view being added
+  # @param [Float] height, the height of the parallax header
+  # @param [Float] min_height, the minimum height of the header when scrolling up
+  # @param [Boolean] shadow, whether to show a shadow along the bottom or not
+  def add_portkey_parallax_with_view(view, height: height, shadow: shadow, min_height: min_height)
+    if portkey_parallax_header
+      portkey_parallax_header.remove_custom_view
+      view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight
+      portkey_parallax_header.custom_view = view
+    else
+      @portkey_parallax_header = PortkeyParallaxHeader::HeaderView.alloc.init_with_frame(
+        CGRectMake(0, 0, self.bounds.size.width, height),
+        custom_view: view,
+        scroll_view: WeakRef.new(self),
+        height:      height,
+        min_height:  min_height,
+        shadow:      shadow
+      )
+      portkey_parallax_header.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight
+      portkey_parallax_header.backgroundColor = UIColor.blackColor
+      self.addSubview(portkey_parallax_header)
+      new_inset           = self.contentInset
+      new_inset.top       = height
+      self.contentInset   = new_inset
+    end
+  end
+
+  def didMoveToSuperview
+    super
+  end
+end
